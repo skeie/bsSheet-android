e25419cc61fe9f358fc50a65c12cddd6d622b1e9
skeie <trulsske@gmail.com>
2020-05-28 10:27:14 +0200

    First go

---
+++ android/app/src/main/java/com/bottomsheet/BSViewManager.java
@@ -0,0 +1,33 @@
+package com.bottomsheet;
+
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.facebook.react.uimanager.ViewGroupManager;
+import com.facebook.react.uimanager.ThemedReactContext;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+public class BSViewManager extends ViewGroupManager<LinearLayout> {
+
+    private final static String REACT_CLASS = "BSBCoordinatorLayoutAndroid";
+    private ThemedReactContext context;
+
+    @Override
+    public String getName() {
+        return REACT_CLASS;
+    }
+
+    @Override
+    public LinearLayout createViewInstance(ThemedReactContext context) {
+        this.context = context;
+         return (LinearLayout) context.getCurrentActivity().getLayoutInflater().inflate(R.layout.bottom_sheet_layout, null);
+
+    }
+
+    @Override
+    public void addView(LinearLayout parent, View child, int index) {
+        BottomSheetDialog mBottomSheetDialog = new BottomSheetDialog(context);
+        mBottomSheetDialog.setContentView(child);
+        mBottomSheetDialog.show();
+    }
+}
